70aa8ff17ebe0c206b931b34a4cd03cb81edec26
алексей загородников <ayayv.ayayayv@yandex.ru>
2024-11-14 12:02:09 +0300

    Домашняя работа по уроку "Перегрузка операторов"

--- House.py
+++ House.py
@@ -20,3 +20,39 @@ class House:
     
     def __str__(self):
         return (f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
+    
+    # Домашняя работа по уроку "Перегрузка операторов"
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+    
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
