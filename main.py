be92e73d8f592815fbac82e1176cb0591b879622
алексей загородников <ayayv.ayayayv@yandex.ru>
2024-11-15 11:54:46 +0300

    Домашняя работа по уроку "Различие атрибутов класса и экземпляра"

---
+++ main.py
@@ -0,0 +1,16 @@
+from module_5_4 import module_5_4
+from module_5_3 import module_5_3
+from module_5_2 import module_5_2
+from module_5_1 import module_5_1
+
+if __name__ == '__main__':
+    
+    module_5_1()
+    # Домашняя работа по уроку "Специальные методы классов"
+    module_5_2()
+    
+    # Домашняя работа по уроку "Перегрузка операторов"
+    module_5_3()
+    
+    #Домашняя работа по уроку "Различие атрибутов класса и экземпляра"
+    module_5_4()
